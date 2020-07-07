127f230cf5235ab33da1d905768d86dfa510eee0
Alan <atang8013@gmail.com>
2020-07-07 15:19:15 -0400

    styled homepage

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavBar = styled.nav`
+  display: flex;
+  text-align: center;
+`;
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
+    <NavBar className="navbar">
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="signup">Sign Up</Link>
       <Link to="/overview">Overview</Link>
-    </nav>
+    </NavBar>
   );
 }
--- client/components/homepage.js
+++ client/components/homepage.js
@@ -1,30 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const Grid = styled.div``;
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const Col = styled.div`
+  flex: ${(props) => props.size};
+`;
 
 const Homepage = () => {
   return (
     <div>
       <h1>Hello</h1>
-      <div className="container">
-        <div className="row">
-          <div className="column">
-            <h1>The new standard in self storage technology</h1>
-            <p>
-              Welcome to storEDGE®. We’re the industry’s most integrated
-              technology solution, with user-friendly software, websites, and
-              online rentals. Our modern, all-in-one technology platform allows
-              you to manage your business seamlessly from anywhere in the world.
-            </p>
-          </div>
-        </div>
-        <div className="double-column">
-          <div>
-            <img
-              className="graphic"
-              src="https://www.storedge.com/images/home/hero.png"
-            />
-          </div>
-        </div>
-      </div>
+      <Grid>
+        <Row>
+          <Col size={1}>The new standard in self storage technology</Col>
+          <Col size={2}>
+            Welcome to storEDGE®. We’re the industry’s most integrated
+            technology solution, with user-friendly software, websites, and
+            online rentals. Our modern, all-in-one technology platform allows
+            you to manage your business seamlessly from anywhere in the world.
+          </Col>
+        </Row>
+      </Grid>
     </div>
   );
 };
